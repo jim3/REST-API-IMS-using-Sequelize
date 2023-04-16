edb6f5203b98d47943d8e10984537291e575fadc
jim3 <j3.casey@protonmail.com>
2023-04-16 12:01:09 -0400

    Update partsRoute.js

--- routes/partsRoute.js
+++ routes/partsRoute.js
@@ -2,22 +2,17 @@ const express = require("express");
 const router = express.Router();
 const partsController = require("../controllers/partsController");
 
-// Render index
-router.get("/", (req, res) => res.render("index"));
+// render index.ejs
+router.get("/", (req, res) => res.render("index")); 
 
-// Routes
+// =-=-=-=-=-=-=-=-=-=-=-=-=- Routes --> Controllers -=-=-=-=-=-=-=-=-=-=-=-=-= //
+
 router.get("/api/parts", partsController.getParts); // get all parts
-
 router.get("/api/parts/:id", partsController.getPart); // get a single part
-
 router.post("/api/parts", partsController.createPart); // create a part
-
 router.put("/api/parts/:id", partsController.updatePart); // update a part
-
 router.delete("/api/parts/:id", partsController.deletePart); // delete a part
-
 router.post("/api/accounts", partsController.createAccount); // create an account
-
 router.get("/api/accounts", partsController.getAccounts); // get all accounts
 
 module.exports = router;
