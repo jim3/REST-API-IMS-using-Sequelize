3509d94bbef65b1703b9ea003666097e65235f3a
jim3 <j3.casey@protonmail.com>
2023-04-16 10:38:14 -0400

    add controller functions

--- controllers/partsController.js
+++ controllers/partsController.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Parts = require("../models/Parts");
 const Accounts = require("../models/Accounts");
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
+// =-=-=-=-=-=-=-=-=-=-=-=-=- PARTS -=-=-=-=-=-=-=-=-=-=-=-=-= //
 
 const getParts = async (req, res) => {
     try {
@@ -17,4 +17,91 @@ const getParts = async (req, res) => {
     }
 };
 
+const getPart = async (req, res) => {
+    try {
+        const part = await db.Parts.findByPk(req.params.id);
+        res.json(part);
+
+        // handle error
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+const createPart = async (req, res) => {
+    try {
+        const { partname, quantity, price, ...product } = req.body;
+        const productType = Object.keys(product)[0];
+        const productValue = product[productType];
+
+        const responseObj = {
+            partName: partname,
+            partType: productValue,
+            quantity,
+            price,
+        };
+        await db.Parts.create(responseObj);
+        res.render("index", {
+            ...responseObj,
+        });
+
+        // handle error
+    } catch (err) {
+        console.error();
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+const updatePart = async (req, res) => {
+    try {
+        const { partname, quantity, price, ...product } = req.body;
+        const productType = Object.keys(product)[0];
+        const productValue = product[productType];
+
+        const responseObj = {
+            partName: partname,
+            partType: productValue,
+            quantity,
+            price,
+        };
+        const part = await db.Parts.findByPk(req.params.id); // get part to update
+
+        // update the part
+        await part.update(responseObj);
+        res.json(responseObj);
+
+        // handle error
+    } catch (err) {
+        console.error();
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+const deletePart = async (req, res) => {
+    try {
+        const { partname, quantity, price, ...product } = req.body;
+        const productType = Object.keys(product)[0];
+        const productValue = product[productType];
+        const responseObj = {
+            partName: partname,
+            partType: productValue,
+            quantity,
+            price,
+        };
+        const part = await db.Parts.findByPk(req.params.id);
+        await part.destroy(responseObj);
+        res.json(responseObj);
+
+        // handle error
+    } catch (err) {
+        console.error();
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=- ACCOUNTS -=-=-=-=-=-=-=-=-=-=-=-=-= //
+
+
 module.exports = router;
+
--- routes/partsRoute.js
+++ routes/partsRoute.js
@@ -2,101 +2,24 @@ const express = require("express");
 const router = express.Router();
 const partsController = require("../controllers/partsController");
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
+// Render index
+router.get("/", (req, res) => res.render("index"));
 
-router.get("/", (req, res) => res.render("index")); // render index page
-
+// Routes
 router.get("/api/parts", partsController.getParts); // get all parts
-
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
-
-router.get("/api/parts/:id", async (req, res) => {
-    try {
-        const part = await db.Parts.findByPk(req.params.id);
-        res.json(part);
-
-        // handle error
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Server error" });
-    }
-});
-
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
-
-router.post("/api/parts", async (req, res) => {
-    try {
-        const { partname, quantity, price, ...product } = req.body;
-        const productType = Object.keys(product)[0];
-        const productValue = product[productType];
-
-        const responseObj = {
-            partName: partname,
-            partType: productValue,
-            quantity,
-            price,
-        };
-        await db.Parts.create(responseObj);
-        res.render("index", {
-            ...responseObj,
-        });
-
-        // handle error
-    } catch (err) {
-        console.error();
-        res.status(500).send("Internal Server Error");
-    }
-});
+router.get("/api/parts/:id", partsController.getPart); // get a single part
+router.post("/api/parts", partsController.createPart); // create a part
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
 
 router.put("/api/parts/:id", async (req, res) => {
-    try {
-        const { partname, quantity, price, ...product } = req.body;
-        const productType = Object.keys(product)[0];
-        const productValue = product[productType];
 
-        const responseObj = {
-            partName: partname,
-            partType: productValue,
-            quantity,
-            price,
-        };
-        const part = await db.Parts.findByPk(req.params.id); // get part to update
-
-        // update the part
-        await part.update(responseObj);
-        res.json(responseObj);
-
-        // handle error
-    } catch (err) {
-        console.error();
-        res.status(500).send("Internal Server Error");
-    }
 });
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
 
 router.delete("/api/parts/:id", async (req, res) => {
-    try {
-        const { partname, quantity, price, ...product } = req.body;
-        const productType = Object.keys(product)[0];
-        const productValue = product[productType];
-        const responseObj = {
-            partName: partname,
-            partType: productValue,
-            quantity,
-            price,
-        };
-        const part = await db.Parts.findByPk(req.params.id);
-        await part.destroy(responseObj);
-        res.json(responseObj);
 
-        // handle error
-    } catch (err) {
-        console.error();
-        res.status(500).send("Internal Server Error");
-    }
 });
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
