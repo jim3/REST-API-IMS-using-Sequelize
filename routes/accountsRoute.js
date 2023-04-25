6cb4d7c3ae4319f33573952eba62f8602f5e95ff
jim3 <j3.casey@protonmail.com>
2023-04-24 22:52:05 -0400

    update

--- routes/accountsRoute.js
+++ routes/accountsRoute.js
@@ -4,7 +4,6 @@ const accountsController = require("../controllers/accountsController");
 const verifyToken = require("../middlewares/verifyToken");
 
 // router.get("/login", (req, res) => res.render("login")); // render login page
-
 router.post("/api/accounts", accountsController.createAccount); // create an account
 router.get("/api/accounts", accountsController.getAccounts); // get all accounts
 router.post("/api/accounts/login", verifyToken, accountsController.login); // login
