f8c23470396eec9fd73a24491d7a41c0a44cd241
jim3 <j3.casey@protonmail.com>
2023-04-24 22:45:20 -0400

    update

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "parts.db",
-  logging: (...msg) => console.log(msg),
+  loggger: console.log,
   define: {
     freezeTableName: true,
   },
--- routes/accountsRoute.js
+++ routes/accountsRoute.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const accountsController = require("../controllers/accountsController");
+const verifyToken = require("../middlewares/verifyToken");
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=- Routes for Accounts -=-=-=-=-=-=-=-=-=-=-=-=-= //
-router.get("/login", (req, res) => res.render("login")); // render login page
+// router.get("/login", (req, res) => res.render("login")); // render login page
 
 router.post("/api/accounts", accountsController.createAccount); // create an account
 router.get("/api/accounts", accountsController.getAccounts); // get all accounts
-router.post("/api/accounts/login", accountsController.login); // login
-router.get("/api/accounts/orders", accountsController.getOrders); // get all orders")
+router.post("/api/accounts/login", verifyToken, accountsController.login); // login
 
 module.exports = router;
