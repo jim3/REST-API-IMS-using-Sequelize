97444264e6002d1626b8c8bd44e79ce869c1d11f
jim3 <j3.casey@protonmail.com>
2023-04-25 17:29:12 -0400

    update

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -5,12 +5,15 @@ dotenv.config();
 
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1]; // split the token from the header
+    // splits the token from the header
+    console.log(authHeader);
+    console.log(authHeader.split(" ")[1]);
+    const token = authHeader && authHeader.split(" ")[1];
 
     if (!token) return res.status(401).send("Access Denied");
 
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET); // verify the token with the `JWT_SECRET` from the .env file
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified;
         next();
     } catch (err) {
